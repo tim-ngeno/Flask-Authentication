54c3761cd53c820c671bf3e8234fcc4d8440abf4
timngeno <h34r54y5@gmail.com>
2021-09-09 15:09:52 +0300

    flask-migrate

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
 from flask_login import LoginManager
 
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app.config['SECRET_KEY'] = '0dcdc1374dcf077130e18a3441127cbc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 bcrypt = Bcrypt(app)
 db = SQLAlchemy(app) 
+migrate = Migrate(app, db)
 
 login_manager = LoginManager(app)
 
